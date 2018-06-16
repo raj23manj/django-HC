0fd512e5759d1810a8ac9f13294c9c8e4e92c357
rajesh23-drg <rmanjunath@teamdrg.com>
2018-06-16 14:00:35 +0530

    product details

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('create', views.create, name='create') 
+    path('create', views.create, name='create'),
+    path('<int:product_id>', views.detail, name='detail') 
 ]
